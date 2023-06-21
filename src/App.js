a283f64d9297e7ddc507e020e609eea4f0140f9a
Mayuri Gurav <mayurigurav1997@gmail.com>
2023-06-21 13:14:22 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
+import { BrowserRoute, Routes, Route, BrowserRouter } from "react-router-dom";
+import { Box } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      Hi JS
-    </div>
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: '#000' }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Feed />} />
+          <Route path="/video/:id" element={<VideoDetail />} />
+          <Route path="/channel/:id" element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm" element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
   );
 }
 
