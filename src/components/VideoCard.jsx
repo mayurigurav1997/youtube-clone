eeae05c6a5db19cb021db9db4435d59d90e0722a
Mayuri Gurav <mayurigurav1997@gmail.com>
2023-06-21 20:08:57 +0530

    Video Cards started

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,8 +1,20 @@
+import { CheckCircle } from '@mui/icons-material'
+import { Typography, Card, CardContent, CardMedia } from '@mui/material'
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { demoThumbnailUrl, demoChannelUrl, demoVideoUrl, demoChannelTitle, demoVideoTitle, demoProfilePicture } from '../utils/constant'
 
-const VideoCard = () => {
+const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
     return (
-        <div>VideoCard</div>
+        <Card>
+            <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+                <CardMedia
+                    image={snippet?.thumbnails?.high?.url}
+                    alt={snippet?.title}
+                    sx={{ width: 358, height: 180 }} />
+
+            </Link>
+        </Card>
     )
 }
 
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,39 +1,23 @@
-import React, { useEffect, useState } from "react";
-import { Box, Stack, Typography } from "@mui/material";
+import { Stack, Box } from '@mui/material'
+import React from 'react'
+import VideoCard from './VideoCard'
+import ChannelCard from './ChannelCard'
 
-import { fetchFromAPI } from "../utils/fetchFromAPI";
-import { Videos, Sidebar } from "./";
-
-const Feed = () => {
-    const [selectedCategory, setSelectedCategory] = useState("New");
-    const [videos, setVideos] = useState(null);
-
-    useEffect(() => {
-        setVideos(null);
-
-        fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
-            .then((data) => setVideos(data.items))
-    }, [selectedCategory]);
+const Videos = ({ videos }) => {
+    console.log(videos, "videos")
 
     return (
-        <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-            <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
-                <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
-
-                <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
-                    Copyright © 2022 JSM Media
-                </Typography>
-            </Box>
-
-            <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
-                <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
-                    {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
-                </Typography>
-
-                <Videos videos={videos} />
-            </Box>
+        <Stack
+            direction='row' flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}
+        >
+            {videos && videos.map((item, idx) => {
+                return (<Box key={idx}>
+                    {item.id.videoId && <VideoCard video={item} />}
+                    {item.id.chanelId && <ChannelCard chanelDetail={item} />}
+                </Box>)
+            })}
         </Stack>
-    );
-};
+    )
+}
 
-export default Feed;
+export default Videos
