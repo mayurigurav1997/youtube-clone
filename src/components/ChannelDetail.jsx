e9ba2ea28960dd7d4afa5d0ecb66c6a4eb15d34d
Mayuri Gurav <mayurigurav1997@gmail.com>
2023-06-22 12:29:55 +0530

    Channel details started

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { Box } from '@mui/material'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import Videos from './Videos'
+import ChannelCard from './ChannelCard'
+import { fetchFromAPI } from '../utils/fetchFromAPI'
 
 const ChannelDetail = () => {
+    const { id } = useParams();
+    const [channelDetail, setChannelDetail] = useState(null)
+    const [videos, setVideos] = useState(null)
+
+    useEffect(() => {
+        fetchFromAPI(`channels?part=snippet&id=${id}"`).
+            then((data) => setChannelDetail(data?.items[0]))
+
+        fetchFromAPI(`search?part=snippet&channelId=${id}&order=date`).
+            then((data) => setVideos(data?.items[0]))
+    }, [id])
     return (
-        <div>ChannelDetail</div>
+        <Box minHeight='95vh' >
+            <Box>
+                <div>
+
+                </div>
+            </Box>
+        </Box >
     )
 }
 
